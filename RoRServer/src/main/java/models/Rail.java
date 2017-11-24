a58deaf994a27bf5aaf2ab92a666308164184e45
Juliane Lies <Juliane.Lies@student.hs-rm.de>
2017-11-25 00:07:26 +0100

    #32-Server_Models

--- RoRServer/src/main/java/models/Rail.java
+++ RoRServer/src/main/java/models/Rail.java
@@ -13,11 +13,15 @@ public class Rail extends InteractiveGameObject implements PlaceableOnSquare {
 	}
 	
 	public Rail (Square square, RailSection section1, RailSection section2) {
-		
+		this.square = square;
+		this.section1 = section1;
+		this.section2 = section2;
 	}
 	
 	public void setPlaceableOnRail (PlaceableOnRail placeableOnRail) {
-		this.placeableOnRail = placeableOnRail;
+		if(placeableOnRail == null){
+			this.placeableOnRail = placeableOnRail;
+		}
 	}
 	
 	
