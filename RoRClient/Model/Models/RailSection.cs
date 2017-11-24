f952032248ea9206e027acf85c887f2adbb39b34
Isabel Rott <isabel.rott@student.hs-rm.de>
2017-11-25 00:43:57 +0100

    #33_Client_Model
    Railsection angepasst.

--- RoRClient/Model/Models/RailSection.cs
+++ RoRClient/Model/Models/RailSection.cs
@@ -8,5 +8,17 @@ namespace RoRClient.Model.Models
 {
     class RailSection
     {
+        private RailSectionPosition node1;
+        private RailSectionPosition node2;
+
+        public RailSection(RailSectionPosition node1, RailSectionPosition node2)
+        {
+            this.node1 = node1;
+            this.node2 = node2;
+        }
+
+        public static readonly RailSection STRAIGHT_VERTICAL = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.SOUTH);
+        public static readonly RailSection STRAIGHT_HORIZONTAL = new RailSection(RailSectionPosition.EAST, RailSectionPosition.WEST);
+        public static readonly RailSection CURVE_NE = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.EAST);
     }
 }
