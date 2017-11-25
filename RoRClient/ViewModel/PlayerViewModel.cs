8a28cb84a14ecae2e4cd62e183c09cde12c7efd7
Jakob Liskow <jakob.liskow@student.hs-rm.de>
2017-11-25 02:40:00 +0100

    (Player Create) button mittles command (MVVM)

---
+++ RoRClient/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.ViewModel
+{
+	class PlayerViewModel
+	{
+		public void CreatePlayerCommand()
+		{
+			Console.WriteLine("create player");
+		}
+	}
+}
