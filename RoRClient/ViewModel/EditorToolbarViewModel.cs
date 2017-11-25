8a159adadcda5a64b5a787ac5e011d88f6d2706e
Hendrik Schaefer <hendrik.schaefer@student.hs-rm.de>
2017-11-25 05:17:49 +0100

    #34-Client: ViewModel verknüpft

---
+++ RoRClient/ViewModel/EditorToolbarViewModel.cs
@@ -0,0 +1,32 @@
+﻿using RoRClient.ViewModel.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.ViewModel
+{
+    class EditorToolbarViewModel : ViewModelBase
+    {
+        private ActionCommand testCommand;
+        public ActionCommand TestCommand
+        {
+            get
+            {
+                if (testCommand == null)
+                {
+                    // 1. Argument: Kommando-Effekt (Execute), 2. Argument: Bedingung "Kommando aktiv?" (CanExecute)
+                    testCommand = new ActionCommand(dummy => this.TestMethode());
+                }
+                return testCommand;
+            }
+        }
+
+        // Hilfsmethode für AddEinkaufCommand
+        private void TestMethode()
+        {
+
+        }
+    }
+}
