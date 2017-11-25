8e29223677adef0be9177a6f443e5c3d0f854617
Isabel Rott <isabel.rott@student.hs-rm.de>
2017-11-25 01:41:16 +0100

    #33_Client_Model
    RailSection ausgebaut.

--- RoRClient/Model/Models/RailSection.cs
+++ RoRClient/Model/Models/RailSection.cs
@@ -8,6 +8,13 @@ namespace RoRClient.Model.Models
 {
     class RailSection
     {
+        public static readonly RailSection STRAIGHT_VERTICAL = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.SOUTH);
+        public static readonly RailSection STRAIGHT_HORIZONTAL = new RailSection(RailSectionPosition.EAST, RailSectionPosition.WEST);
+        public static readonly RailSection CURVE_NE = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.EAST);
+        public static readonly RailSection CURVE_ES = new RailSection(RailSectionPosition.EAST, RailSectionPosition.SOUTH);
+        public static readonly RailSection CURVE_SW = new RailSection(RailSectionPosition.SOUTH, RailSectionPosition.WEST);
+        public static readonly RailSection CURVE_WN = new RailSection(RailSectionPosition.WEST, RailSectionPosition.NORTH);
+
         private RailSectionPosition node1;
         private RailSectionPosition node2;
 
@@ -16,9 +23,5 @@ namespace RoRClient.Model.Models
             this.node1 = node1;
             this.node2 = node2;
         }
-
-        public static readonly RailSection STRAIGHT_VERTICAL = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.SOUTH);
-        public static readonly RailSection STRAIGHT_HORIZONTAL = new RailSection(RailSectionPosition.EAST, RailSectionPosition.WEST);
-        public static readonly RailSection CURVE_NE = new RailSection(RailSectionPosition.NORTH, RailSectionPosition.EAST);
     }
 }
