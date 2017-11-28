3651ac090d88ee0cf4af3a8c2f0890bbbfa249f5
Micha Lanvers <micha.lanvers@student.hs-rm.de>
2017-11-28 16:25:50 +0100

    #36-Client: BaseKlasse für CanvasUserControll erstellt

---
+++ RoRClient/View/CanvasUserControl.cs
@@ -0,0 +1,80 @@
+﻿using RoRClient.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RoRClient.View
+{
+    public class CanvasUserControl : UserControl
+    {
+        public int X
+        {
+            get
+            {
+                return (int)GetValue(XProperty);
+            }
+            set
+            {
+                SetValue(XProperty, value);
+            }
+        }
+        public static readonly DependencyProperty XProperty = DependencyProperty.Register("X", typeof(int), typeof(CanvasUserControl), new UIPropertyMetadata(0, OnXchanged));
+
+        private static void OnXchanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CanvasUserControl CanvasUserControl = (CanvasUserControl)d;
+            CanvasUserControl.RealX = CanvasUserControl.X * ViewConstants.SQUARE_DIM;
+        }
+
+
+        public int Y
+        {
+            get
+            {
+                return (int)GetValue(YProperty);
+            }
+            set
+            {
+                SetValue(YProperty, value);
+            }
+        }
+        public static readonly DependencyProperty YProperty = DependencyProperty.Register("Y", typeof(int), typeof(CanvasUserControl), new UIPropertyMetadata(0, OnYchanged));
+
+        private static void OnYchanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CanvasUserControl CanvasUserControl = (CanvasUserControl)d;
+            CanvasUserControl.RealY = CanvasUserControl.Y * ViewConstants.SQUARE_DIM;
+        }
+
+        public int RealX
+        {
+            get
+            {
+                return (int)GetValue(RealXProperty);
+            }
+            set
+            {
+                SetValue(RealXProperty, value);
+            }
+        }
+        public static readonly DependencyProperty RealXProperty = DependencyProperty.Register("RealX", typeof(int), typeof(CanvasUserControl), new UIPropertyMetadata(0));
+
+        public int RealY
+        {
+            get
+            {
+                return (int)GetValue(RealYProperty);
+            }
+            set
+            {
+                SetValue(RealYProperty, value);
+            }
+        }
+        public static readonly DependencyProperty RealYProperty = DependencyProperty.Register("RealY", typeof(int), typeof(CanvasUserControl), new UIPropertyMetadata(0));
+
+    }
+}
--- RoRClient/View/SquareUserControl.xaml.cs
+++ RoRClient/View/SquareUserControl.xaml.cs
@@ -4,76 +4,14 @@ using System.Windows.Controls;
 
 namespace RoRClient.View
 {
-    public partial class SquareUserControl : UserControl
+    public partial class SquareUserControl : CanvasUserControl
     {
         public SquareUserControl()
         {
             InitializeComponent();
             SquareDim = ViewConstants.SQUARE_DIM;
         }
-        public int X
-        {
-            get
-            {
-                return (int)GetValue(XProperty);
-            }
-            set
-            {
-                SetValue(XProperty, value);
-            }
-        }
-        public static readonly DependencyProperty XProperty = DependencyProperty.Register("X", typeof(int), typeof(SquareUserControl), new UIPropertyMetadata(0, OnXchanged));
-
-        private static void OnXchanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            SquareUserControl squareUserControl = (SquareUserControl)d;
-            squareUserControl.RealX = squareUserControl.X * ViewConstants.SQUARE_DIM;
-        }
-
-        public int RealX
-        {
-            get
-            {
-                return (int)GetValue(XProperty);
-            }
-            set
-            {
-                SetValue(RealXProperty, value);
-            }
-        }
-        public static readonly DependencyProperty RealXProperty = DependencyProperty.Register("RealX", typeof(int), typeof(SquareUserControl), new UIPropertyMetadata(0));
-
-        public int Y
-        {
-            get
-            {
-                return (int)GetValue(YProperty);
-            }
-            set
-            {
-                SetValue(YProperty, value);
-            }
-        }
-        public static readonly DependencyProperty YProperty = DependencyProperty.Register("Y", typeof(int), typeof(SquareUserControl), new UIPropertyMetadata(0, OnYchanged));
-
-        private static void OnYchanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            SquareUserControl squareUserControl = (SquareUserControl)d;
-            squareUserControl.RealY = squareUserControl.Y * ViewConstants.SQUARE_DIM;
-        }
-        public int RealY
-        {
-            get
-            {
-                return (int)GetValue(YProperty);
-            }
-            set
-            {
-                SetValue(RealYProperty, value);
-            }
-        }
-        public static readonly DependencyProperty RealYProperty = DependencyProperty.Register("RealY", typeof(int), typeof(SquareUserControl), new UIPropertyMetadata(0));
-
+        
         public int SquareDim
         {
             get
