15a16498eebbaecc91b44760c0ceb58d5ec6bf50
Isabel Rott <isabel.rott@student.hs-rm.de>
2017-11-29 12:28:05 +0100

    #36_Client
    Änderungen auf Server für ModelId

--- RoRServer/src/main/java/models/InteractiveGameObject.java
+++ RoRServer/src/main/java/models/InteractiveGameObject.java
@@ -1,10 +1,10 @@
 package models;
 
 /**
- * Abstrakte Klasse für alle Objekte, mit denen interagiert werden kann
+ * Abstrakte Klasse fuer alle Objekte, mit denen interagiert werden kann
  *
  */
-public abstract class InteractiveGameObject {
+public abstract class InteractiveGameObject extends ModelBase {
 	protected Square square;
 	
 	public InteractiveGameObject(Square square) {
