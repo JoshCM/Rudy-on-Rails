70d86636e689fbe3be7cb87df11492d7e6e9f619
Florian Treder <florian.treder@student.hs-rm.de>
2017-11-29 15:07:58 +0100

    #34 #35 Refactoring ViewOrdnerstruktur
    Da später noch seeeehr viele Usercontrolls hinzugefügt werden, haben wir vorab geclustert.

--- RoRClient/View/Editor/EditorToolbarUserControl.xaml.cs
+++ RoRClient/View/Editor/EditorToolbarUserControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RoRClient.View
+namespace RoRClient.View.Editor
 {
     /// <summary>
     /// Interaktionslogik für EditorToolbarUserControl.xaml
