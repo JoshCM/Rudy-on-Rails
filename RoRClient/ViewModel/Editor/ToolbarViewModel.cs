6945bfe1b520ab5d8345296cf0065ea876b777b3
Florian Treder <florian.treder@student.hs-rm.de>
2017-11-29 15:30:45 +0100

    #34 #35 Dummyklasse für Werkzeuge für Testzwecke
    Umsetzung in View fehlt definitiv.

---
+++ RoRClient/ViewModel/Editor/ToolbarViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoRClient.Model.EditorModels;
+using RoRClient.ViewModel.Helper;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace RoRClient.ViewModel.Editor
+{
+    class ToolbarViewModel : ObservableObjectBaseClass
+    {
+        private readonly Werkzeug _textConverter = new Werkzeug(s => s.ToUpper());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(SomeText)) return;
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = string.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
