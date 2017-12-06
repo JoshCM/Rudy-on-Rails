dfafe6f361a96f7db9407992d0083f0e10f0dd83
Florian Treder <florian.treder@student.hs-rm.de>
2017-12-07 00:12:29 +0100

    Refactoring: ftred001
    Habe meine Dummyklassen entfernt

--- RoRClient/ViewModel/Editor/ToolbarViewModel.cs
+++ RoRClient/ViewModel/Editor/ToolbarViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RoRClient.Model.EditorModels;
 using RoRClient.ViewModel.Helper;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
