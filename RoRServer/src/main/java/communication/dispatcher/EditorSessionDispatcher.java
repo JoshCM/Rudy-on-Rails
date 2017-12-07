5ae57aabc889dee6543177d1828bd3483a665ff7
Jakob Liskow <jakob.liskow@student.hs-rm.de>
2017-12-07 18:37:39 +0100

    #55-Server: Bugfix(permant SOUTH und NORTH jeder Rail vom Dispatcher aus)

--- RoRServer/src/main/java/communication/dispatcher/EditorSessionDispatcher.java
+++ RoRServer/src/main/java/communication/dispatcher/EditorSessionDispatcher.java
@@ -35,7 +35,7 @@ public class EditorSessionDispatcher extends DispatcherBase {
 		Map map = editorSession.getMap();
 		Square square = map.getSquare(xPos, yPos);
 		
-		Rail rail = new Rail(square, RailSectionPosition.NORTH, RailSectionPosition.SOUTH);
+		Rail rail = new Rail(square, railSectionPositionNode1, railSectionPositionNode2);
 		square.setPlaceable(rail);
 	}
 }
