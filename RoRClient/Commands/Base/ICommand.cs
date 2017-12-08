a66a4279aab8f88a31ab479ca33c7290f17906ab
Andreas Pöhler <andreas.poehler@student.hs-rm.de>
2017-12-08 12:55:53 +0100

    #55-Client: Ordnerstruktur für Commands / ICommand, CommandBase erstellt

---
+++ RoRClient/Commands/Base/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RoRClient.Commands.Base
+{
+
+    /// <summary>
+    /// Basis-Interface für Commands
+    /// </summary>
+    public interface ICommand
+    {
+        void execute();
+    }
+}
