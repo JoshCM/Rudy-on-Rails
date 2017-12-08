a51cec4ab9d97e3e5d7d29eaf866e0d23289c3d5
Andreas Pöhler <andreas.poehler@student.hs-rm.de>
2017-12-08 14:50:09 +0100

    #55-Client: Bearbeitung Commands

--- RoRClient/Commands/Base/CommandBase.cs
+++ RoRClient/Commands/Base/CommandBase.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RoRClient.Models.Editor;
+using RoRClient.Communication.DataTransferObject;
 
 namespace RoRClient.Commands.Base
 {
     public abstract class CommandBase : ICommand
     {
-        public void execute()
+
+        protected EditorSession session;
+
+        public CommandBase(EditorSession session, MessageInformation message)
+        {
+            this.session = session;
+        }
+
+        public void Execute()
         {
             throw new NotImplementedException();
         }
