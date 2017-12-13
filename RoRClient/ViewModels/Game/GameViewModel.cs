3be3821f2ad3d96a3adb93b5524cede67ee8b245
Andreas Pöhler <andreas.poehler@student.hs-rm.de>
2017-12-13 13:51:09 +0100

    #52-Client: Dampf-Modus View über Button anzeigen

---
+++ RoRClient/ViewModels/Game/GameViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RoRClient.ViewModels.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.ViewModels.Game
+{
+    class GameViewModel : ViewModelBase
+    {
+        UIState uiState;
+        public GameViewModel(UIState uiState)
+        {
+            this.uiState = uiState;
+        }
+    }
+}
