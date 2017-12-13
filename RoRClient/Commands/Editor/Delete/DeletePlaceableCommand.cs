a91cc1de066e98aa2bcb83c9cc0d39a94d0bcd6b
HendrikSchaefer <hendrik.schaefer@student.hs-rm.de>
2017-12-13 17:28:20 +0100

    Merge branch 'develop' into feature/73_74_75_Delete_Rail_Editor

---
+++ RoRClient/Commands/Editor/Delete/DeletePlaceableCommand.cs
@@ -0,0 +1,33 @@
+﻿using RoRClient.Commands.Base;
+using RoRClient.Communication.DataTransferObject;
+using RoRClient.Models.Game;
+using RoRClient.Models.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.Commands.Editor.Delete
+{
+    public class DeletePlaceableCommand : CommandBase
+    {
+
+        private Guid squareId;
+        private int xPos;
+        private int yPos;
+
+        public DeletePlaceableCommand(GameSession session, MessageInformation messageInformation) : base(session, messageInformation)
+        {
+            xPos = messageInformation.GetValueAsInt("xPos");
+            yPos = messageInformation.GetValueAsInt("yPos");
+        }
+
+        public override void Execute()
+        {
+            GameSession editorSession = GameSession.GetInstance();
+            Square square = editorSession.Map.GetSquare(xPos, yPos);
+            square.PlaceableOnSquare = null;
+        }
+    }
+}
