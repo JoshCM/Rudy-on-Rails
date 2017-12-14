1fa1de97e980472eecc0c45d7e0fd329486da3ff
Jakob Liskow <jakob.liskow@student.hs-rm.de>
2017-12-15 00:26:03 +0100

    #63-Server: TrainstationRails sind nicht mehr löschbar und rotierbar

---
+++ RoRServer/src/main/java/commands/editor/RotateTrainstationCommand.java
@@ -0,0 +1,20 @@
+package commands.editor;
+
+import commands.base.CommandBase;
+import communication.MessageInformation;
+import models.session.RoRSession;
+
+public class RotateTrainstationCommand extends CommandBase{
+
+	public RotateTrainstationCommand(RoRSession session, MessageInformation messageInfo) {
+		super(session, messageInfo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
