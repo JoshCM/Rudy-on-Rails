a9284cd2311ed79a17dc75fed5aeff1184b14d01
Christian Lohrey <christian.lohrey@student.hs-rm.de>
2017-12-19 12:27:07 +0100

    #59-Client Command für Geschwindigkeitsregler an Binding gehängt

---
+++ RoRClient/ViewModels/Game/SpeedSliderViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoRClient.Commands.Base;
+
+namespace RoRClient.ViewModels.Game
+{
+    class SpeedSliderViewModel
+    {
+        private ICommand updateSpeedCommand;
+        internal ICommand UpdateSpeedCommand(double newValue)
+        {
+            sendUpdateSpeedCommand(newValue);
+
+            return updateSpeedCommand;
+        }
+
+        private void sendUpdateSpeedCommand(double newValue)
+        {
+            Console.WriteLine("neuer Wert:" + newValue);
+        }
+    }
+}
