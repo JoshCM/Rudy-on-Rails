713b35aec5bd1db169bf851a5adf007e45552c92
Micha Lanvers <micha.lanvers@student.hs-rm.de>
2017-12-21 17:31:42 +0100

    Refactoring: UnitTests für handleCreateGameSession

--- RoRServer/src/test/java/communication/FromClientRequestQueueDispatcherTests.java
+++ RoRServer/src/test/java/communication/FromClientRequestQueueDispatcherTests.java
@@ -12,6 +12,8 @@ import helper.MessageQueueStub;
 import models.game.Player;
 import models.session.EditorSession;
 import models.session.EditorSessionManager;
+import models.session.GameSession;
+import models.session.GameSessionManager;
 
 public class FromClientRequestQueueDispatcherTests {
 	@Test
@@ -56,6 +58,52 @@ public class FromClientRequestQueueDispatcherTests {
 		assertEquals(messageType, messageEnvelope.getMessageType());
 		assertEquals(editorSessionName, response.getValueAsString("editorName"));
 		assertEquals(editorSessionName, response.getValueAsString("topicName"));
+		assertEquals(playerName, response.getValueAsString("playerName"));
+		assertNotNull(response.getValueAsUUID("playerId"));
+	}
+	
+	@Test
+	public void FromClientRequestQueueDispatcher_handleCreateGameSession_createsGameSessionAndHostPlayer() {
+		MessageQueueStub messageQueueStub = new MessageQueueStub();
+		FromClientRequestQueueDispatcher dispatcher = new FromClientRequestQueueDispatcher();
+		dispatcher.addObserver(messageQueueStub);
+		String messageType = "CreateGameSession";
+		String gameSessionName = "TestSession";
+		String playerName = "MyPlayer";
+
+		MessageInformation messageInfo = new MessageInformation(messageType);
+		messageInfo.putValue("gameName", gameSessionName);
+		messageInfo.putValue("playerName", playerName);
+		messageInfo.setClientid(UUID.randomUUID().toString());
+		dispatcher.handleCreateGameSession(messageInfo);
+
+		GameSession editorSession = GameSessionManager.getInstance().getGameSessionByName(gameSessionName);
+		assertNotNull(editorSession);
+		
+		Player player = editorSession.getPlayers().get(0);
+		assertNotNull(player);
+	}
+
+	@Test
+	public void FromClientRequestQueueDispatcher_handleCreateGameSession_createsResponseWithExpectedValues() {
+		MessageQueueStub messageQueueStub = new MessageQueueStub();
+		FromClientRequestQueueDispatcher dispatcher = new FromClientRequestQueueDispatcher();
+		dispatcher.addObserver(messageQueueStub);
+		String messageType = "CreateGameSession";
+		String gameSessionName = "TestSession";
+		String playerName = "MyPlayer";
+
+		MessageInformation messageInfo = new MessageInformation(messageType);
+		messageInfo.putValue("gameName", gameSessionName);
+		messageInfo.putValue("playerName", playerName);
+		messageInfo.setClientid(UUID.randomUUID().toString());
+		dispatcher.handleCreateGameSession(messageInfo);
+
+		MessageEnvelope messageEnvelope = messageQueueStub.messages.get(0);
+		MessageInformation response = messageEnvelope.getMessageInformation();
+		assertEquals(messageType, messageEnvelope.getMessageType());
+		assertEquals(gameSessionName, response.getValueAsString("gameName"));
+		assertEquals(gameSessionName, response.getValueAsString("topicName"));
 		assertEquals(playerName, response.getValueAsString("playerName"));
 		assertNotNull(response.getValueAsUUID("playerId"));
 	}
