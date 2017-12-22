08fef1e128158e613edeff0284f6f4e95974a61a
Juliane Lies <juliane.lies@student.hs-rm.de>
2017-12-22 12:49:53 +0100

    #98-Client: GameViewModels für Container, Gold, Coal erstellt

---
+++ RoRClient/ViewModels/Game/GoldGameViewModel.cs
@@ -0,0 +1,29 @@
+﻿using RoRClient.Models.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.ViewModels.Game
+{
+    class GoldGameViewModel : CanvasGameViewModel
+    {
+        private Gold g;
+        public GoldGameViewModel(Gold gold) : base(gold.Id)
+        {
+            this.g = gold;
+            this.SquarePosX = gold.Square.PosX;
+            this.SquarePosY = gold.Square.PosY;
+        }
+
+
+        public Gold gold
+        {
+            get
+            {
+                return g;
+            }
+        }
+    }
+}
