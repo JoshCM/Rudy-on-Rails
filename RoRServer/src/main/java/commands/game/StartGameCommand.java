590f931b425abf84c8faa37786890d814ca61c08
Andreas Pöhler <andreas.poehler@student.hs-rm.de>
2017-12-22 15:27:55 +0100

    #82 - Client: Binding für TrainstationUserControl

--- RoRServer/src/main/java/commands/game/StartGameCommand.java
+++ RoRServer/src/main/java/commands/game/StartGameCommand.java
@@ -33,7 +33,7 @@ public class StartGameCommand extends CommandBase {
 		
 		// Wenn der MapName für das Laden mitgeschickt wird
 		// String mapName = messageInfo.getValueAsString("mapName");
-		String mapName = "NeueTolleMap";
+		String mapName = "GameDefaultMap";
 		
 		// Map laden
 		Map map = MapManager.loadMap(mapName);
