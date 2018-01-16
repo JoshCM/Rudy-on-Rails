c61430fb44d98bad4b027f253d7412256dc3c6af
Jakob Liskow <jakob.liskow@student.hs-rm.de>
2018-01-16 14:39:33 +0100

    #105-Server-Client_Resourcen_in_StatusBar: Resourcen bisher nicht ohne sie auf ein Square setzen zu müssen erzeugbar

--- RoRClient/Models/Session/GameSession.cs
+++ RoRClient/Models/Session/GameSession.cs
@@ -3,12 +3,8 @@ using RoRClient.Communication.Dispatcher;
 using RoRClient.Communication.Topic;
 using RoRClient.Models.Game;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace RoRClient.Models.Session
 {
