bc9eabe5bdfa5bbfa7dd8efc0af8e6a6cb796c22
Micha Lanvers <micha.lanvers@student.hs-rm.de>
2018-01-18 20:49:30 +0100

    #227-Client: Styles von Buttons, ComboBox(Items) ausgelagert in Styles.xaml, sodass sie wiederverwendet werden können. Die ComboBox für Geisterzug-Scripts wird jetzt vom Server gefüllt.

---
+++ RoRClient/ViewModels/Game/GameInteractionsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using RoRClient.Models.Game;
+using RoRClient.Models.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoRClient.ViewModels.Game
+{
+    public class GameInteractionsViewModel : ViewModelBase
+    {
+        public Scripts Scripts
+        {
+            get
+            {
+                return GameSession.GetInstance().Scripts;
+            }
+        }
+    }
+}
