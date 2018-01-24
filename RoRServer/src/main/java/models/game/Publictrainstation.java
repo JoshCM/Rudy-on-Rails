bccd14af261623944c049bc4b51cc73eeff017e2
Isabel Rott <isabel.rott@student.hs-rm.de>
2018-01-24 18:12:49 +0100

    173_Server: Tausch mit Loco

--- RoRServer/src/main/java/models/game/Publictrainstation.java
+++ RoRServer/src/main/java/models/game/Publictrainstation.java
@@ -56,50 +56,85 @@ public class Publictrainstation extends Trainstation {
 	public String toString() {
 		return "PublicTrainstation [trainstationRailIds=" + trainstationRailIds + ", alignment=" + alignment + "]";
 	}
+	
+	private void locoAtPublictrainstation() {
+		for (Rail rail : this.getTrainstationRails()) {
+			Square trainstationSquare = rail.getSquareFromGameSession();
+			if (trainstationSquare != null) {
+				if (trainstationSquare.getPlaceableOnSquare() instanceof PlayerLoco) {
+					Loco loco = (Loco) trainstationSquare.getPlaceableOnSquare();
+					exchangeResource(loco);
+				}
+			}
+		}
+	}
 
-	private void exchangeResource() {
-		// ToDo: Beim Cart der Loco muss analysiert werden,
-		// welche Resource er haelt.
-		// Daraufhin muss dann nach der zum Verhaeltnis passenden Resource
-		// in der Trainstation gesucht werden,
-		// diese wird aus dem Stock entfernt, der Loco hinzugefuegt und
-		// die Resource der Loco wird dem Stock hinzugefuegt
+	private void exchangeResource(Loco loco) {		
 		gameSession = GameSessionManager.getInstance().getGameSessionByName(sessionName);
-		List<Loco> locos = gameSession.getLocos();
-		for (Loco loco : locos) {
-			List<Cart> carts = loco.getCarts();
-			for (Cart cart : carts) {
-				if (cart.getXPos() == this.getXPos() && cart.getYPos() == this.getYPos()
-						&& cart.getResource() != null && stock.getResources().size() != 0) {
-					// Die Resource der Cart
-					String resourceType = cart.getResource().getDescription();
-					
-					// Die Resources im Stock der Trainstation
-					List<Resource> resources = stock.getResources();
-					for (Resource resource : resources) {
-						if (resourceType == "Gold" && resource.getDescription() == "Coal") {
-							// 1 Gold : 1 Kohle
-							Resource r = cart.getResource();
-							cart.removeResourceFromCart();
-							resources.remove(resource);
-							resources.add(r);
-							cart.loadResourceOntoCart(resource);
-							
-							notifyResourceExchanged(resourceType);
-						}
-						if (resourceType == "Coal" && resource.getDescription() == "Gold") {
-							// 3 Kohle : 1 Gold
-							if (carts.size() == 3 /* && 3x Coal */) {
-								for (Cart c : carts) {
+		GamePlayer player = (GamePlayer) gameSession.getPlayerById(loco.getPlayerId());
+		List<Cart> carts = loco.getCarts();
+		for (Cart cart : carts) {
+			if (cart.getXPos() == this.getXPos() && cart.getYPos() == this.getYPos()
+					&& cart.getResource() != null && stock.getResources().size() != 0) {
+				
+				// Die Resource der Cart
+				String resourceType = cart.getResource().getDescription();
+				
+				// Die Resources im Stock der Trainstation
+				List<Resource> resources = stock.getResources();
+				
+				for (Resource resource : resources) {
+					if (resourceType == "Gold" && resource.getDescription() == "Coal" /* && ich will Kohle */) {
+						// 1 Gold : 1 Kohle
+						Resource r = cart.getResource();
+						cart.removeResourceFromCart();
+						resources.remove(resource);
+						resources.add(r);
+						cart.loadResourceOntoCart(resource);
+						
+						notifyResourceExchanged(resourceType);
+						
+						// Playerkonto aktualisieren
+						player.removeGold(1);
+						player.addCoal(1);
+					}
+					if (resourceType == "Coal" && resource.getDescription() == "Gold" /* && ich will Gold */) {
+						// 3 Kohle : 1 Gold
+						if (carts.size() >= 3 /* && 3x Coal */) {
+							for (Cart c : carts) {
+								if (c.getResource().getDescription() == "Coal") {
 									Resource r = c.getResource();
 									c.removeResourceFromCart();
 									resources.add(r);
 								}
-								resources.remove(resource);
-								cart.loadResourceOntoCart(resource);
-								
-								notifyResourceExchanged(resourceType);
 							}
+							resources.remove(resource);
+							cart.loadResourceOntoCart(resource);
+							
+							notifyResourceExchanged(resourceType);
+							
+							// Playerkonto aktualisieren
+							player.removeCoal(3);
+							player.addGold(1);
+						}
+					}
+					if (resourceType == "Gold" /* && ich will Punkte */) {
+						// 2 Gold : 1 Punkte
+						if (carts.size() >= 2 /* && 2x Gold */) {
+							for (Cart c : carts) {
+								if (c.getResource().getDescription() == "Gold") {
+									Resource r = c.getResource();
+									c.removeResourceFromCart();
+									resources.add(r);
+								}
+							}
+							// Wie werden Punkte in Trainstation gehändelt?
+							
+							notifyResourceExchanged(resourceType);
+							
+							//Playerkonto aktualisieren
+							player.removeGold(2);
+							player.addPoints(1);
 						}
 					}
 				}
