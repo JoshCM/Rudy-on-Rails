5cfa61c99800c017521205486b9d3f9d66dc0b03
Jana Labenz <jana.labenz@student.hs-rm.de>
2018-01-29 18:20:18 +0100

    #19 Fremde Switches nicht verstellen, Anzeige Punkte anderer spieler

--- RoRClient/ViewModels/Game/ScoreboardViewModel.cs
+++ RoRClient/ViewModels/Game/ScoreboardViewModel.cs
@@ -37,7 +37,7 @@ namespace RoRClient.ViewModels.Game
         {
             foreach (GamePlayer player in PlayerList)
             {
-                Console.WriteLine("Name" + player.Name + " Kohle " + player.CoalCount);
+                Console.WriteLine("Name" + player.Name + " Punkte " + player.PointCount);
             }
 
             if (Opacity == 1.0)
